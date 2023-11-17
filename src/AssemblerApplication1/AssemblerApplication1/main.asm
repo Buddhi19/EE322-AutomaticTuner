096989f86b45dba56a880019b47e58465d964ca0
Buddhi Wijenayake <wijenayakebuddhi34802@gmail.com>
2023-11-17 20:55:54 +0530

    added 3ldr off/on states, added stepper motor

--- src/AssemblerApplication1/AssemblerApplication1/main.asm
+++ src/AssemblerApplication1/AssemblerApplication1/main.asm
@@ -6,4 +6,38 @@
 ;			Athulya Ratnayake
 ;			Thimira Hirushan
 
+.include "m328pdef.inc"
+	
+	.def	ldrhigh = r17							; define higher frequency indicating ldr
+	.def	ldrlow  = r18							; define lower frequency indicating ldr
+	.def	ldrok   = r19							; define accepted frequecy indicating ldr
+	.def	temp    = r20							; define temporary register
 
+	.cseg 
+	.org	0x00									; set instruction starting address to 0x00
+	ldi 	temp, (1<<PB0) | (1<<PB1) | (1<<PB2)	; set temp register such that it can manage all 3ldrs
+	out		DDRB, temp
+	
+setup:
+	rcall	lowldron
+	rjmp	setup
+
+lowldron:
+	cbi		PORTB, 0
+	cbi		PORTB, 1
+	sbi		PORTB, 2
+	ret
+
+okldron:
+	cbi		PORTB, 0
+	cbi		PORTB, 2
+	sbi		PORTB, 1
+	ret
+
+highldron:
+	cbi		PORTB, 1
+	cbi		PORTB, 2
+	sbi		PORTB, 0
+	ret
+
+	
