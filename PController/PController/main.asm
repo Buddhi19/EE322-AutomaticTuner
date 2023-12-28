245ab77ee193ccd6570da19f2c83910568863088
Athulya Ratnayake <83506647+athulya24453@users.noreply.github.com>
2023-12-29 00:12:52 +0530

    p_controller added (not yet debugged)

---
+++ PController/PController/main.asm
@@ -0,0 +1,42 @@
+;
+; PController.asm
+;
+; Created: 12/28/2023 11:24:35 PM
+; Author : Humble Orbit
+
+.include "m328pdef.inc"
+
+.def r30, p_con
+.def r31, comp
+
+.org 0x00
+	jmp start
+
+start:
+    sbrs crosscounterH, 0 ;0th bit of crosscounterH is set => value read > 255
+	rjmp low ; jump to low loop
+	rjmp high ; jump to high loop
+	rjmp start
+
+low:
+	ser comp ; se all bits of comp
+	sub comp, crosscounterL ; subtract value read from the desired value
+	breq doneL
+	mov p_con, comp ; move the difference to p_con register
+	lsr p_con
+	lsr p_con ; divide difference by 4, kp=1/4
+	doneL:
+		rjmp start ; kill the loop if equal
+	rjmp low
+
+
+high:
+	clr comp ; clear all bits of comp
+	sub comp, crosscounterL ; subtract value read from the desired value
+	breq doneH
+	mov p_con, comp ; move the difference to p_con register
+	lsr p_con
+	lsr p_con ; divide difference by 4, kp=1/4
+	doneH:
+		rjmp start ; kill the loop if equal
+	rjmp high
